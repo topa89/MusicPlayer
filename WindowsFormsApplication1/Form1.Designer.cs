e80d7f9a765d61f1b72ee42882411a91f71af8d5
Anton Dranitsyn <anton-dranicyn@mail.ru>
2017-02-01 14:45:25 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonRand = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.buttonPlayPause = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 226);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.White;
+            this.listBox1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(6, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(412, 202);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Location = new System.Drawing.Point(10, 244);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(420, 170);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.AutoSize = true;
+            this.button1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.Search;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(17, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 45);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // buttonRand
+            // 
+            this.buttonRand.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonRand.AutoSize = true;
+            this.buttonRand.BackColor = System.Drawing.Color.Transparent;
+            this.buttonRand.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRand.BackgroundImage")));
+            this.buttonRand.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRand.FlatAppearance.BorderSize = 0;
+            this.buttonRand.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRand.Location = new System.Drawing.Point(261, 31);
+            this.buttonRand.Name = "buttonRand";
+            this.buttonRand.Size = new System.Drawing.Size(45, 45);
+            this.buttonRand.TabIndex = 6;
+            this.buttonRand.UseVisualStyleBackColor = false;
+            this.buttonRand.Click += new System.EventHandler(this.buttonRand_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPrev.AutoSize = true;
+            this.buttonPrev.BackColor = System.Drawing.Color.Transparent;
+            this.buttonPrev.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonPrev.BackgroundImage")));
+            this.buttonPrev.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonPrev.FlatAppearance.BorderSize = 0;
+            this.buttonPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPrev.Location = new System.Drawing.Point(68, 26);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(55, 55);
+            this.buttonPrev.TabIndex = 3;
+            this.buttonPrev.UseVisualStyleBackColor = false;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonNext.AutoSize = true;
+            this.buttonNext.BackColor = System.Drawing.Color.Transparent;
+            this.buttonNext.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.Next;
+            this.buttonNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonNext.FlatAppearance.BorderSize = 0;
+            this.buttonNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNext.Location = new System.Drawing.Point(200, 26);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(55, 55);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.UseVisualStyleBackColor = false;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(39, 50);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(325, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // buttonPlayPause
+            // 
+            this.buttonPlayPause.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPlayPause.AutoSize = true;
+            this.buttonPlayPause.BackColor = System.Drawing.Color.Transparent;
+            this.buttonPlayPause.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonPlayPause.BackgroundImage")));
+            this.buttonPlayPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonPlayPause.FlatAppearance.BorderSize = 0;
+            this.buttonPlayPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlayPause.Location = new System.Drawing.Point(129, 19);
+            this.buttonPlayPause.Name = "buttonPlayPause";
+            this.buttonPlayPause.Size = new System.Drawing.Size(65, 65);
+            this.buttonPlayPause.TabIndex = 0;
+            this.buttonPlayPause.TabStop = false;
+            this.buttonPlayPause.UseVisualStyleBackColor = false;
+            this.buttonPlayPause.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox3.Controls.Add(this.buttonRand);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.buttonNext);
+            this.groupBox3.Controls.Add(this.buttonPlayPause);
+            this.groupBox3.Controls.Add(this.buttonPrev);
+            this.groupBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox3.Location = new System.Drawing.Point(39, 74);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(325, 90);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(438, 419);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(360, 400);
+            this.Name = "Form1";
+            this.Text = "Музыка";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonPlayPause;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonRand;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo arr;
+        Random randomNumber = new Random();
+        FileInfo[] fi;
+        bool checkPicture = true;
+        int indexMusic = 0;
+        int timeMusic;
+        string minutes, seconds, aPath;
+        
+      KrasimirTrapper.MP3Player mp = new KrasimirTrapper.MP3Player();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            aPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            SearchMusic(aPath);
+   
+        }
+
+        private void SearchMusic(string path)
+        {
+            arr = new DirectoryInfo(path);
+            fi = arr.GetFiles("*.mp3");
+            //добавляем список файлов в listview
+            foreach (FileInfo fc in fi) listBox1.Items.Add(fc.Name);
+            label1.Text = "0:00";
+            label2.Text = "0:00";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!mp.IsPaused)
+            {   
+                buttonPlayPause.BackgroundImage = ((Properties.Resources.PlayMusic));
+                mp.Pause();
+                timer1.Stop();
+            }
+            else
+            {
+                buttonPlayPause.BackgroundImage = ((Properties.Resources.Pause));
+                mp.Play();
+                timer1.Start();
+                listBox1.SelectedIndex = indexMusic;
+            }
+            
+        }
+
+        private void Play(int index) 
+        {
+            listBox1.SelectedIndex = index;
+            trackBar1.Value = 0;
+            buttonPlayPause.BackgroundImage = ((Properties.Resources.Pause));
+           mp.Open(Path.Combine(Application.StartupPath, arr.FullName + "\\" + fi[index])); //открываем трек
+
+            trackBar1.Maximum = (int)(mp.AudioLength / 1000); //длина трека
+            if (((int)mp.AudioLength / 1000) % 60 < 10) seconds = "0" + ((int)(mp.AudioLength / 1000 % 60)).ToString();
+            else seconds = ((int)(mp.AudioLength / 1000 % 60)).ToString();
+            label1.Text = ((int)(mp.AudioLength / 1000 / 60)).ToString() + ":" + seconds; //время
+            timeMusic = (int)(mp.AudioLength / 1000);
+            mp.Play();
+            trackBar1.Maximum = timeMusic;
+            timer1.Start();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mp.Play();
+            indexMusic = listBox1.SelectedIndex;
+            Play(indexMusic);
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (checkPicture)
+            {
+                indexMusic += 1;
+                if (indexMusic >= fi.Length)
+                {
+                    mp.Play();
+                    indexMusic = 0;
+                    Play(indexMusic);
+                }
+                else
+                {
+                    mp.Play();
+                    label1.Text = indexMusic.ToString();
+                    listBox1.SelectedIndex = indexMusic;
+                    Play(indexMusic);
+                }
+            }
+            else
+            {
+                indexMusic = randomNumber.Next(0, fi.Length - 1);
+                Play(indexMusic);
+            }
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (indexMusic == 0)
+            {
+                listBox1.SelectedIndex = indexMusic;
+                Play(indexMusic);
+            }
+            else
+            {
+                indexMusic -= 1;
+                label1.Text = indexMusic.ToString();
+                Play(indexMusic);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (((int)(mp.CurrentPosition / 1000) == 0) && (trackBar1.Value > 0))
+            {
+                mp.Stop();
+                timer1.Stop();
+                if (checkPicture) indexMusic += 1;
+                else indexMusic = randomNumber.Next(0, fi.Length);
+                Play(indexMusic);
+            }
+            else
+            { 
+                trackBar1.Value = (int)(mp.CurrentPosition / 1000);
+                minutes = (((int)(mp.CurrentPosition / 1000)) / 60).ToString();
+                if (((int)(mp.CurrentPosition / 1000)) % 60 < 10) seconds = "0" + (((int)(mp.CurrentPosition / 1000)) % 60).ToString();
+                else { seconds = (((int)(mp.CurrentPosition / 1000)) % 60).ToString(); }
+                label2.Text = minutes + ":" + seconds;
+            }
+        }
+
+        private void buttonRand_Click(object sender, EventArgs e)
+        {
+            if (!checkPicture)
+            {
+                buttonRand.BackgroundImage = ((Properties.Resources.ShuffleOff));
+                checkPicture = true;
+            }
+            else { buttonRand.BackgroundImage = ((Properties.Resources.Shuffle)); checkPicture = false; }
+
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            mp.Seek((ulong)(trackBar1.Value * 1000));
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.Description = "Выберите папку, \n" + "в которой находится музыка";
+            fb.ShowNewFolderButton = false;
+            fb.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                aPath = fb.SelectedPath;
+                SearchMusic(aPath);
+            }
+        }
+    }
+
+   
+}
+
